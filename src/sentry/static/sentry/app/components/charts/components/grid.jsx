7484ade7f66b67c66cd294392a9ddcf55d27458a
Ayesha <ayesha.omarali@gmail.com>
2018-09-11 16:57:30 -0700

    reduce l/r padding in charts to 5%
    keep early adopter message at the bottom of page
    
    adjusted grid defaults, added basic legend
    
    lint error
    
    Add legend to sentryTypes, change truncate to 80
    
    removed containLabel bc it cuts off graphs randomly
    
    Added legend scroll, moved truncation to inside legend component
    
    make legend data dynamic, use SentryTypes

--- src/sentry/static/sentry/app/components/charts/components/grid.jsx
+++ src/sentry/static/sentry/app/components/charts/components/grid.jsx
@@ -5,16 +5,13 @@
  */
 export default function Grid(props = {}) {
   return {
-    top: 24,
-
-    // Is this a decent default for X-axis labels?
+    top: 40,
     bottom: 40,
-
     // This should allow for sufficient space for Y-axis labels
-    left: '10%',
-
-    right: '10%',
-
+    left: '3%',
+    right: '3%',
+    padding: 5,
+    itemGap: 25,
     ...props,
   };
 }
--- src/sentry/static/sentry/app/components/charts/components/legend.jsx
+++ src/sentry/static/sentry/app/components/charts/components/legend.jsx
@@ -1,8 +1,17 @@
 import 'echarts/lib/component/legend';
+import 'echarts/lib/component/legendScroll';
 
 export default function Legend(props = {}) {
   return {
     show: true,
+    type: 'scroll',
+    formatter: function(name) {
+      if (name.length > 80) {
+        return name.substring(0, 80) + '...';
+      } else {
+        return name;
+      }
+    },
     ...props,
   };
 }
