83593d36cf3b2e89a29420d22df9d4e9a34a7ead
Ayesha <ayesha.omarali@gmail.com>
2018-09-17 19:37:16 -0700

    added padding:0 to grid

--- src/sentry/static/sentry/app/components/charts/components/legend.jsx
+++ src/sentry/static/sentry/app/components/charts/components/legend.jsx
@@ -5,6 +5,7 @@ export default function Legend(props = {}) {
   return {
     show: true,
     type: 'scroll',
+    padding: 0,
     formatter: function(name) {
       if (name.length > 80) {
         return name.substring(0, 80) + '...';
